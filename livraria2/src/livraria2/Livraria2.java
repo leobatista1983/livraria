d495a43097751dc882b7c0c906b01fedeaf7bb5f
Francisco Ferreira da Costa Júnior <costajuniorbr@gmail.com>
2014-09-12 18:37:35 -0300

    Update Livraria2.java

--- livraria2/src/livraria2/Livraria2.java
+++ livraria2/src/livraria2/Livraria2.java
@@ -15,6 +15,32 @@ public class Livraria2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("ola mundo");
+
+    private String idLivro;
+    private String titulo;
+    private String autores;
+    private int ano;
+    private double preco;
+    private int quantidade;
+    private String descricao;
+
+    public Livro() {
+    }
+
+    public String getIdLivro() {
+        return idLivro;
+    }
+
+    public void setIdLivro(String idLivro) {
+        this.idLivro = idLivro;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
     }
 }
